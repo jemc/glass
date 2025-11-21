60f83d883a83b66e6b77a48c19ce3ba070978848
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2025-11-21 15:31:18 -0800

    Refactor Agate.SpawnOnStatus to accept multiple status configs.

--- packages/pyrope/src/Spawn.ts
+++ packages/pyrope/src/Spawn.ts
@@ -6,17 +6,14 @@ import { Context } from "./Context"
 type SpawnFn = (world: World, pyrope: Context, position: Opal.Position) => void
 
 interface SpawnConfig {
+  readonly fn: SpawnFn
   readonly positionOffset?: ReadVector2
 }
 
 export class SpawnOnStatus {
   static readonly componentId = registerComponent(this)
 
-  constructor(
-    readonly statusName: string,
-    readonly fn: SpawnFn,
-    readonly config: SpawnConfig = {},
-  ) {}
+  constructor(readonly map: Record<string, SpawnConfig>) {}
 }
 
 export const SpawnOnStatusSystem = (pyrope: Context) =>
@@ -24,20 +21,21 @@ export const SpawnOnStatusSystem = (pyrope: Context) =>
     shouldMatchAll: [SpawnOnStatus],
 
     runEach(entity, spawn, status, position) {
-      const { statusName, fn, config } = spawn
       const { coords, scale: direction } = position
 
-      if (status.isStarting(statusName)) {
-        fn(
-          pyrope.world,
-          pyrope,
-          new Opal.Position(
-            coords.x + (config.positionOffset?.x ?? 0) * direction.x,
-            coords.y + (config.positionOffset?.y ?? 0) * direction.y,
-            direction.x,
-            direction.y,
-          ),
-        )
+      for (const [statusName, config] of Object.entries(spawn.map)) {
+        if (status.isStarting(statusName)) {
+          config.fn(
+            pyrope.world,
+            pyrope,
+            new Opal.Position(
+              coords.x + (config.positionOffset?.x ?? 0) * direction.x,
+              coords.y + (config.positionOffset?.y ?? 0) * direction.y,
+              direction.x,
+              direction.y,
+            ),
+          )
+        }
       }
     },
   })
